90807ed782d5efa81ac3762a9e98eaaa5c777ed5
PankajB3 <pankajbansal@rapidinnovation.dev>
2022-03-22 18:42:56 +0530

    added test cases for the mintVillage

---
+++ test/TinyVillage.js
@@ -0,0 +1,26 @@
+// const { isOfType } = require('@celo/contractkit/lib/identity/claims/claim');
+const {expect} = require('chai');
+const { ethers } = require("hardhat");
+
+describe('NFT Test', () => { 
+    it('TC-1. Test mintVillage deployement',async()=>{
+        // const [user] = await ethers.getSigners();
+        const Token = await ethers.getContractFactory("TinyVillage");
+        const tkn = Token.deploy();
+        expect(tkn).to.be.ok();
+    })
+    it('TC-2 Test mintVillage mint', async()=>{
+        const [user] = await ethers.getSigners();
+        const Token = await ethers.getContractFactory("TinyVillage");
+        const tkn = await Token.deploy();
+       await tkn.mintVillage();
+       expect((await tkn.balanceOf(user.address,0)).toNumber()).to.equal(1);
+    })
+    it('TC-3 Test only one village should get minted', async()=>{
+        const [user] = await ethers.getSigners();
+        const Token = await ethers.getContractFactory("TinyVillage");
+        const tkn = await Token.deploy();
+       await tkn.mintVillage();
+    //    expect((await tkn.balanceOf(user.address,0)).toNumber()).to.equal(1);
+    })
+ })
