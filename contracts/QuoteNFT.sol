0f831086c7bcc8832c64b76c2d47bfcb53b860af
PankajB3 <pankajbansal@rapidinnovation.dev>
2022-04-07 11:22:14 +0530

    added QuoteNFT.sol

---
+++ contracts/QuoteNFT.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier:MIT
+
+pragma solidity >=0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract QuoteNFT is ERC721{
+    constructor() ERC721("QuoteNFT","QNFT"){}
+
+    
+}
