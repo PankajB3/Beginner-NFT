ad7e84000b5f11f5c74cb4e88d29a763f8585a0b
PankajB3 <pankajbansal@rapidinnovation.dev>
2022-04-07 15:43:43 +0530

    minor changes

--- contracts/QuoteNFT.sol
+++ contracts/QuoteNFT.sol
@@ -13,11 +13,15 @@ contract QuoteNFT is ERC721URIStorage {
 
     modifier onlyOwner(address user) {
         require(user == owner, "Only Owner can call");
-       _;
+        _;
     }
-// give base extension without baseUri
-    constructor(string memory name_, string memory symbol_, string memory _baseExtension) ERC721(name_, symbol_)
-    {
+
+    // give base extension without baseUri
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        string memory _baseExtension
+    ) ERC721(name_, symbol_) {
         name = name_;
         symbol = symbol_;
         owner = msg.sender;
@@ -27,18 +31,13 @@ contract QuoteNFT is ERC721URIStorage {
     function mint() public onlyOwner(msg.sender) {
         _mint(msg.sender, _tokenId);
         _setTokenURI(_tokenId, baseExtension);
+        tokenURI(_tokenId);
         _tokenId++;
     }
 
     // https://ipfs.io/ipfs/Qmba8o49D2ycRRUNVPdVBgtkF4XiBRAKQ1e4eqJTUHWMrb?filename=NFT1.png
     // method overriden to provide my NFT sharable link as baseURI
-    function _baseURI()
-        internal
-        view
-        virtual
-        override
-        returns (string memory)
-    {
+    function _baseURI() internal view virtual override returns (string memory) {
         string memory baseIPFS = "https://ipfs.io/ipfs/";
         // string memory URI = string(abi.encodePacked(baseIPFS, baseExtension));
         return baseIPFS;
