0260ef30d46b85a20e8c53a8ab80eec4d2ca0ad1
PankajB3 <pankajbansal@rapidinnovation.dev>
2022-04-08 10:32:47 +0530

    data structure added to marketplace

--- contracts/MarketPlace.sol
+++ contracts/MarketPlace.sol
@@ -2,11 +2,13 @@
 
 pragma solidity >=0.8.0;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
 contract MarketPlace {
     uint256 private counter = 0;
 
     // creating market data structure
-    struct marketItem{
+    struct marketItem {
         uint256 id;
         uint256 tokenId;
         uint256 price;
@@ -18,7 +20,11 @@ contract MarketPlace {
     // mapping to track market items
     mapping(uint256 => marketItem) private marketIndex;
 
-    function listMyNFT(address nftToken, uint256 tokenId, uint256 price) external{
+    function listMyNFT(
+        address nftToken,
+        uint256 tokenId,
+        uint256 price
+    ) external {
         marketIndex[counter] = marketItem(
             counter,
             tokenId,
@@ -26,11 +32,13 @@ contract MarketPlace {
             nftToken,
             payable(msg.sender),
             payable(address(0))
-        )
+        );
+        counter++;
+        // providing apprioval to market place tyo sell my nft token.
+        IERC721(nftToken).setApprovalForAll(address(this), true);
     }
 
-    function buyNft(uint256 _tokenId) external{
+    function buyNft(uint256 _tokenId) external {
         require();
     }
-
 }
