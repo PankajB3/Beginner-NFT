f6ecca0ad47f425be7c2c8d5b860e44c088f68b0
PankajB3 <pankajbansal@rapidinnovation.dev>
2022-04-08 15:36:46 +0530

    minor changes done. NFT Marketplace functional

--- contracts/MarketPlace.sol
+++ contracts/MarketPlace.sol
@@ -28,19 +28,22 @@ contract MarketPlace {
         address nftToken,
         uint256 tokenId,
         uint256 price
-    ) external {
-        marketIndex[counter] = marketItem(
+    ) external payable{
+        // create instance for this marketItem, add values = > then push in aaray, not directly access index.
+        marketIndex.push(marketItem(
             counter,
             tokenId,
             price,
             nftToken,
             payable(msg.sender),
             payable(address(0))
-        );
+        ));
         counter++;
-        // providing apprioval to market place tyo sell my nft token.
-        IERC721(nftToken).setApprovalForAll(address(this), true);
+        // providing apprioval to market place tyo sell my nft token, call this fx from QuoteNFT itself.
+        // IERC721(nftToken).setApprovalForAll(address(this), true);
     }
+
+    receive() external payable{}
 
     function buyNft(uint256 _tokenId) external payable {
         // require(_exists(_tokenId),"Token ID does not exist");
@@ -48,17 +51,20 @@ contract MarketPlace {
         // require(m);
         (bool f, ) = m.creator.call{value: m.price}("");
         require(f, "Payment failed");
+        m.buyer = payable(msg.sender);
         IERC721(m.nftToken).transferFrom(m.creator, m.buyer, m.tokenId);
     }
 
     function exist(uint256 _tId) internal view returns (marketItem memory) {
         uint256 marketLen = marketIndex.length;
+        marketItem memory m1;
         for (uint256 i = 0; i < marketLen; i++) {
-            marketItem memory m1 = marketIndex[i];
+            m1 = marketIndex[i];
             uint256 nftToken = m1.tokenId;
             if (_tId == nftToken) {
                 return m1;
             }
         }
+        return m1;
     }
 }
