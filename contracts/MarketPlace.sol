f6b70bcb269b2901d94754c252b702f87e2fbea6
PankajB3 <pankajbansal@rapidinnovation.dev>
2022-04-08 11:56:43 +0530

    added exist, buyNFT fx.

--- contracts/MarketPlace.sol
+++ contracts/MarketPlace.sol
@@ -2,7 +2,7 @@
 
 pragma solidity >=0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 contract MarketPlace {
     uint256 private counter = 0;
@@ -18,7 +18,10 @@ contract MarketPlace {
     }
 
     // mapping to track market items
-    mapping(uint256 => marketItem) private marketIndex;
+    marketItem[] private marketIndex;
+    // mapping(uint256 => marketItem) private marketIndex;
+
+    constructor() {}
 
     function listMyNFT(
         address nftToken,
@@ -38,7 +41,24 @@ contract MarketPlace {
         IERC721(nftToken).setApprovalForAll(address(this), true);
     }
 
-    function buyNft(uint256 _tokenId) external {
-        require();
+    function buyNft(uint256 _tokenId) external payable{
+        // require(_exists(_tokenId),"Token ID does not exist");
+        marketItem memory m = exist(_tokenId);
+        // require(m);
+       (bool f,) =  m.creator.call{value:m.price}("");
+
+       require(f,"Payment failed");
+
+    }
+
+    function exist(uint256 _tId) internal view returns (marketItem memory) {
+        uint256 marketLen = marketIndex.length;
+        for (uint256 i = 0; i < marketLen; i++) {
+            marketItem memory m1 = marketIndex[i];
+            uint256 nftToken = m1.tokenId;
+            if (_tId == nftToken) {
+                return m1;
+            }
+        }
     }
 }
